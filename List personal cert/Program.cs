b4875e201e7270b4d9ac270c1fd33cde28bfb722
VO LE HUy <volehuy1998@gmail.com>
2021-09-10 00:09:33 +0700

    Inital commit

---
+++ List personal cert/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_personal_cert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // step 1. generate personal certificate from https://stackoverflow.com/a/201277
+            // step 2. install with Current user
+
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+
+            store.Open(OpenFlags.ReadOnly);
+
+            foreach (X509Certificate2 certificate in store.Certificates)
+            {
+                Console.WriteLine(certificate.Issuer);
+            }
+        }
+    }
+}
