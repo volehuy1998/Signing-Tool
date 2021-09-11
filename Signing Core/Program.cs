bee48a4f2b3c0776e0e5d4b8fd94fe51ff9247f7
VO LE HUy <volehuy1998@gmail.com>
2021-09-11 23:47:00 +0700

    Add Mircosoft Sign XML & WinAPI list personal certificates

---
+++ Signing Core/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+
+namespace Signing_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create a new CspParameters object to specify
+                // a key container.
+                CspParameters cspParams = new CspParameters();
+                cspParams.KeyContainerName = "XML_DSIG_RSA_KEY";
+
+                // Create a new RSA signing key and save it in the container.
+                //RSACryptoServiceProvider.UseMachineKeyStore = true;
+                RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider(cspParams);
+                 // Create a new XML document.
+                 XmlDocument xmlDoc = new XmlDocument();
+
+                // Load an XML file into the XmlDocument object.
+                xmlDoc.PreserveWhitespace = true;
+                xmlDoc.Load("test.xml");
+
+                // Sign the XML document.
+                PureSignXml(xmlDoc, rsaKey);
+                //bool res = PureVerifyXml(xmlDoc, rsaKey);
+
+                //Console.WriteLine("XML file signed.");
+
+                // Save the document.
+                xmlDoc.Save("test_signed.xml");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // Sign an XML file.
+        // This document cannot be verified unless the verifying
+        // code has the key with which it was signed.
+        public static void PureSignXml(XmlDocument xmlDoc, RSA rsaKey)
+        {
+            // Check arguments.
+            if (xmlDoc == null)
+                throw new ArgumentException(nameof(xmlDoc));
+            if (rsaKey == null)
+                throw new ArgumentException(nameof(rsaKey));
+
+            // Create a SignedXml object.
+            SignedXml signedXml = new SignedXml(xmlDoc);
+
+            // Add the key to the SignedXml document.
+            signedXml.SigningKey = rsaKey;
+
+            // Create a reference to be signed.
+            Reference reference = new Reference();
+            reference.Uri = "";
+
+            // Add an enveloped transformation to the reference.
+            XmlDsigEnvelopedSignatureTransform env = new XmlDsigEnvelopedSignatureTransform();
+            reference.AddTransform(env);
+
+            // Add the reference to the SignedXml object.
+            signedXml.AddReference(reference);
+
+            // Compute the signature.
+            signedXml.ComputeSignature();
+
+            // Get the XML representation of the signature and save
+            // it to an XmlElement object.
+            XmlElement xmlDigitalSignature = signedXml.GetXml();
+
+            // Append the element to the XML document.
+            xmlDoc.DocumentElement.AppendChild(xmlDoc.ImportNode(xmlDigitalSignature, true));
+        }
+
+        // Verify the signature of an XML file against an asymmetric
+        // algorithm and return the result.
+        public static Boolean PureVerifyXml(XmlDocument xmlDoc, RSA key)
+        {
+            // Check arguments.
+            if (xmlDoc == null)
+                throw new ArgumentException("xmlDoc");
+            if (key == null)
+                throw new ArgumentException("key");
+
+            // Create a new SignedXml object and pass it
+            // the XML document class.
+            SignedXml signedXml = new SignedXml(xmlDoc);
+
+            // Find the "Signature" node and create a new
+            // XmlNodeList object.
+            XmlNodeList nodeList = xmlDoc.GetElementsByTagName("Signature");
+
+            // Throw an exception if no signature was found.
+            if (nodeList.Count <= 0)
+            {
+                throw new CryptographicException("Verification failed: No Signature was found in the document.");
+            }
+
+            // This example only supports one signature for
+            // the entire XML document.  Throw an exception
+            // if more than one signature was found.
+            if (nodeList.Count >= 2)
+            {
+                throw new CryptographicException("Verification failed: More that one signature was found for the document.");
+            }
+
+            // Load the first <signature> node.
+            signedXml.LoadXml((XmlElement)nodeList[0]);
+
+            // Check the signature and return the result.
+            return signedXml.CheckSignature(key);
+        }
+    }
+}
